4f103ebb9034351efcf7c25bc8468454aaa4a978
Park-12 <sohyunp96@gmail.com>
2021-06-24 23:59:06 +0900

    게시물 작성 완료

--- src/com/shp/exam/app/App.java
+++ src/com/shp/exam/app/App.java
@@ -1,5 +1,7 @@
 package com.shp.exam.app;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import com.shp.exam.app.dto.Article;
@@ -11,24 +13,31 @@ public class App {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		List<Article> articles = new ArrayList<>();
+		int articlesLastId = 0;
+		
 		while (true) {
 			System.out.print("명령어) ");
 			String command = sc.nextLine().trim();
 			
 			if (command.equals("/usr/article/write")) {
-				System.out.print("제목 : ");
+				System.out.printf("제목 : ");
 				String title = sc.nextLine().trim();
-				System.out.print("내용 : ");
+				System.out.printf("내용 : ");
 				String body = sc.nextLine().trim();
 				
 				Article article = new Article();
-				article.id = 1;
+				article.id = articlesLastId + 1;
 				article.regDate = "2021-06-24 12:12:12";
 				article.updateDate = "2021-06-24 12:12:12";
 				article.title = title;
 				article.body = body;
-				System.out.println(article);
-				break;
+				
+				articles.add(article);
+				
+				articlesLastId++;
+				
+				System.out.printf("%d번 게시물이 생성되었습니다.\n", article.id);				
 			}
 			
 			if (command.equals("/usr/system/exit")) {
--- src/com/shp/exam/app/dto/Article.java
+++ src/com/shp/exam/app/dto/Article.java
@@ -1,16 +1,15 @@
 package com.shp.exam.app.dto;
 
-public class Article extends Object {
+public class Article {
 	public int id;
 	public String regDate;
 	public String updateDate;
 	public String title;
 	public String body;
+
 	@Override
 	public String toString() {
 		return "Article [id=" + id + ", regDate=" + regDate + ", updateDate=" + updateDate + ", title=" + title
 				+ ", body=" + body + "]";
 	}
-	
-	
 }
