d932d69b6a7b87b8150873e98715c6f0731dd803
Park-12 <sohyunp96@gmail.com>
2021-06-24 23:34:22 +0900

    Article 클래스 추가

--- src/com/shp/exam/app/App.java
+++ src/com/shp/exam/app/App.java
@@ -2,26 +2,42 @@ package com.shp.exam.app;
 
 import java.util.Scanner;
 
+import com.shp.exam.app.dto.Article;
+
 public class App {
 
 	public static void run() {
-		System.out.println("== 텍스트 게시판 시작 ==");
+		System.out.println("== 코틀린 텍스트 게시판 시작 ==");
 		
-		Scanner sc = new java.util.Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
 		while (true) {
 			System.out.print("명령어) ");
-			
 			String command = sc.nextLine().trim();
 			
-			if(command.equals("/usr/system/exit")) {
+			if (command.equals("/usr/article/write")) {
+				System.out.print("제목 : ");
+				String title = sc.nextLine().trim();
+				System.out.print("내용 : ");
+				String body = sc.nextLine().trim();
+				
+				Article article = new Article();
+				article.id = 1;
+				article.regDate = "2021-06-24 12:12:12";
+				article.updateDate = "2021-06-24 12:12:12";
+				article.title = title;
+				article.body = body;
+				System.out.println(article);
+				break;
+			}
+			
+			if (command.equals("/usr/system/exit")) {
 				System.out.println("프로그램을 종료합니다.");
 				break;
 			}
 		}
 		
-		System.out.println("== 텍스트 게시판 끝 ==");
-		
+		System.out.println("== 코틀린 텍스트 게시판 끝 ==");
 	}
 
 }
---
+++ src/com/shp/exam/app/dto/Article.java
@@ -0,0 +1,16 @@
+package com.shp.exam.app.dto;
+
+public class Article extends Object {
+	public int id;
+	public String regDate;
+	public String updateDate;
+	public String title;
+	public String body;
+	@Override
+	public String toString() {
+		return "Article [id=" + id + ", regDate=" + regDate + ", updateDate=" + updateDate + ", title=" + title
+				+ ", body=" + body + "]";
+	}
+	
+	
+}
