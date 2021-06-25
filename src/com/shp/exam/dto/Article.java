e7dcfb338ce2345ab2d98bec8e807586ec4a16f0
Park-12 <sohyunp96@gmail.com>
2021-06-25 17:46:01 +0900

    게시물 삭제

---
+++ src/com/shp/exam/dto/Article.java
@@ -0,0 +1,15 @@
+package com.shp.exam.dto;
+
+public class Article {
+	public int id;
+	public String regDate;
+	public String updateDate;
+	public String title;
+	public String body;
+
+	@Override
+	public String toString() {
+		return "Article [id=" + id + ", regDate=" + regDate + ", updateDate=" + updateDate + ", title=" + title
+				+ ", body=" + body + "]";
+	}
+}
